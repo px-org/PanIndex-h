1e350f163afb65c4168c1efce02ac3745c6e3c4b
libosong <libosong@guttv.cn>
2021-02-09 11:56:33 +0800

    update

---
+++ heroku/getPanIndex.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+version="v1.0.2"
+if [ "$version" = "" ]
+then
+    version=`curl --silent "https://api.github.com/repos/libsgh/PanIndex/releases/latest" \
+        | grep '"tag_name":' \
+        | sed -E 's/.*"([^"]+)".*/\1/'`
+fi
+curl -sOL "https://github.com/libsgh/PanIndex/releases/download/${version}/PanIndex-${version}-linux-amd64.tar.gz"
+md5sum "PanIndex-"${version}"-linux-amd64.tar.gz"
+tar -xvzf "PanIndex-"${version}"-linux-amd64.tar.gz"
+rm -rf README.md LICENSE
+chmod +x PanIndex
