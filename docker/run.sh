aa4f3620792fd837e997020c3d7111935b60aa49
libsgh <woiyyng@gmail.com>
2022-03-16 10:47:08 +0800

    :test_tube:version test

--- docker/run.sh
+++ docker/run.sh
@@ -6,11 +6,10 @@ then
         | grep '"tag_name":' \
         | sed -E 's/.*"([^"]+)".*/\1/'`
 fi
-echo $version
 curl -sOL "https://github.com/libsgh/PanIndex/releases/download/${version}/PanIndex-linux-amd64.tar.gz"
-md5sum "PanIndex-"${version}"-linux-amd64.tar.gz"
-tar -zxvf "PanIndex-"${version}"-linux-amd64.tar.gz"
+md5sum "PanIndex-linux-amd64.tar.gz"
+tar -zxvf "PanIndex-linux-amd64.tar.gz"
 rm -rf README.md LICENSE
-mv "PanIndex-"${version}"-linux-amd64" PanIndex
+mv PanIndex-linux-amd64 PanIndex
 chmod +x PanIndex
 /app/PanIndex
